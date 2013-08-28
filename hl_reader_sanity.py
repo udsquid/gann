686399dd39ca35a88d87713b2813017ff85de277
udsquid <udsquid.altair@icloud.com>
2013-08-28 08:01:52 +0800

    Add messages to show what are we doing.

--- hl_reader_sanity.py
+++ hl_reader_sanity.py
@@ -28,15 +28,24 @@ middle = records[average]
 last = records[upper]
 
 # check them all
+# check the first record
 t, p = first
+print "Checking the first record.. ",
 assert t == '1990/02/12', TIME_ERR % "first"
 assert p == '12682', PRICE_ERR % "first"
+print "OK"
+# check middle one
+print "Checking the middle record.. ",
 t, p = middle
 assert t == '1998/09/01', TIME_ERR % "middle"
 assert p == '6219', PRICE_ERR % "middle"
+print "OK"
+# check the last one
+print "Checking the last record.. ",
 t, p = last
 assert t == '2008/05/20', TIME_ERR % "last"
 assert p == '9309', PRICE_ERR % "last"
+print "OK"
 
 # pass!
 print "PASS!!"
