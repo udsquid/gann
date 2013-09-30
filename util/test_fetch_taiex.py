9cd57bca8929e67e3713d456cf870e4f7ddc0caf
udsquid <udsquid.altair@icloud.com>
2013-09-30 08:40:09 +0800

    Improve test framework

--- util/test_fetch_taiex.py
+++ util/test_fetch_taiex.py
@@ -3,14 +3,14 @@
 """Test functionalities of fetch_taiex.py.
 
 Usage:
-    test_fetch_taiex.py --unit-test
-    test_fetch_taiex.py (-h | --help)
-    test_fetch_taiex.py (-v | --version)
+    test_fetch_taiex.py [--type=<type>]
+    test_fetch_taiex.py -h | --help
+    test_fetch_taiex.py -v | --version
 
 Options:
+    --type=<type>      Type of testing [default: unit-test].
     -h, --help         Show this screen.
     -v, --version      Show version.
-    --unit-test        Test basic functions of fetch_taiex.py.
 """
 
 ###
@@ -26,18 +26,47 @@ from fetch_taiex import *
 
 
 ###
+### variables
+###
+test_result = dict( # p: pass; f:fail; n: not run; s: skip
+    old = dict(regular = 'n', holiday = 'n'),
+)
+
+
+###
 ### unit test functions
 ###
+def test_fetch_old_regular():
+    try:
+        print "\tFetching indexes in regular day.."
+        result = fetch_single1(date(2000, 1, 4))
+        check_list = [
+            ('09:00', '8448.84'),
+            ('09:01', '8644.91'),
+            ('09:02', '8683.23')]
+        for k, v in check_list:
+            assert result[k] == v, \
+                "index at '%s' should be '%s', not '%s'" % (k, v, result[k])
+        return 'p'
+    except Exception as e:
+        print "** Error:", e
+        return 'f'
+
+
+def test_fetch_old_holiday():
+    try:
+        print "\tFetching indexes in holiday.."
+        result = fetch_single1(date(2000, 1, 8))
+        assert not result, "indexes in '2000-01-08' should be empty"
+        return 'p'
+    except Exception as e:
+        print "** Error:", e
+        return 'f'
+
+
 def test_fetch_old():
-    r1 = fetch_single1(date(2000, 1, 4))
-    assert r1['09:00'] == '8448.84', \
-        "index at '09:00' should be '8448.84', not '%s'" % r1['09:00']
-    assert r1['09:01'] == '8644.91', \
-        "index at '09:01' should be '8644.91', not '%s'" % r1['09:01']
-    assert r1['09:02'] == '8683.23', \
-        "index at '09:02' should be '8683.23', not '%s'" % r1['09:02']
-    r2 = fetch_single1(date(2000, 1, 8))
-    assert not r2, "indexes in '2000-01-08' should be empty"
+    test_result['old']['regular'] = test_fetch_old_regular()
+    test_result['old']['holiday'] = test_fetch_old_holiday()
 
 
 ###
@@ -45,7 +74,14 @@ def test_fetch_old():
 ###
 if __name__ == '__main__':
     args = docopt(__doc__, version="test_fetch_taiex.py 1.0")
-    if args['--unit-test']:
-        print "Testing fetch from old site (every 1 minute).. "
-        test_fetch_old()
-        print "OK"
+    print "Testing fetch from old site (every 1 minute).. "
+    test_fetch_old()
+    results = test_result['old'].values()
+    print ""
+    print "Pass: %d, Fail: %d, Skip: %d" % (results.count('p'),
+                                            results.count('f'),
+                                            results.count('s'))
+    if results.count('p') == len(results):
+        print "OK!"
+    else:
+        print "** Fail"
