ccfc2a9dd7cf07fee2bc8c083b22018f288a1828
udsquid <udsquid.altair@icloud.com>
2013-10-12 16:01:38 +0800

    Complete 'count' and 'verify' modules

--- util/verify_fetch_single2.py
+++ util/verify_fetch_single2.py
@@ -3,8 +3,8 @@
 """Verify tool for fetch_taiex.py -- fetch_single2.
 
 Usage:
-    verify_fetch_single2.py verify [--start=<start>] [--end=<end>]
-    verify_fetch_single2.py count (total | trade | holiday)
+    verify_fetch_single2.py verify html [--start=<start>] [--end=<end>]
+    verify_fetch_single2.py count days [--start=<start>] [--end=<end>]
     verify_fetch_single2.py -h | --help
     verify_fetch_single2.py -v | --version
 
@@ -40,28 +40,44 @@ def setup_date_boundaries(start, end):
     return start, end
 
 
+def _fetch_one(day):
+    url = 'http://www.twse.com.tw/ch/trading/exchange/MI_5MINS_INDEX/genpage/Report{year}{month:02d}/A121{year}{month:02d}{day:02d}.php?chk_date={taiex_date}'.format(year=day.year, month=day.month, day=day.day, taiex_date=convert_date(day))
+    resp = requests.get(url)
+    return PyQuery(decode_page(resp.text))
+
+
 ###
 ### test functions
 ###
 def verify_html(start, end):
-    """Verify the HTML structure day by day are keep the same."""
+    """Verify the HTML structures day by day are keep the same."""
     print "Verifying.. "
+    flag = start.replace(year=start.year-1, month=1, day=1)
     for day in days_range(start, end):
-        url = 'http://www.twse.com.tw/ch/trading/exchange/MI_5MINS_INDEX/genpage/Report{year}{month:02d}/A121{year}{month:02d}{day:02d}.php?chk_date={taiex_date}'.format(year=day.year, month=day.month, day=day.day, taiex_date=convert_date(day))
-        resp = requests.get(url)
-        data = PyQuery(decode_page(resp.text))
+        if flag.year != day.year:
+            print day.year
+            flag = day
+        data = _fetch_one(day)
+        if u'查無資料' in data('body').text():
+            continue
+        if not data('table.board_trad') and \
+           not data('div#tbl-container'):
+            print "verify success until %s" % day
+            break
 
 
-def count_total(start, end):
-    return (end - start).days + 1
-
-
-def count_trade(start, end):
-    return 0
-
-
-def count_holiday(start, end):
-    return 0
+def count_days(start, end):
+    total = (end - start).days + 1
+    trade = holiday = 0
+    for day in days_range(start, end):
+        data = _fetch_one(day)
+        if u'查無資料' in data('body').text():
+            holiday += 1
+        else:
+            trade += 1
+    assert total == (trade + holiday), \
+        "days number are not match"
+    print "Total: %d, trade: %d, holiday: %d" % (total, trade, holiday)
 
 
 ###
@@ -73,19 +89,10 @@ if __name__ == '__main__':
     start = normalize_date(args['--start'])
     end = normalize_date(args['--end'])
     start, end = setup_date_boundaries(start, end)
-    if not start or not end:
-        print "*** Error: Date range is not correct!"
-        exit(ERROR_INVALID_ARGUMENT)
+    assert start or end, "*** Error: Date range is not correct!"
     # start verifying
     if args['verify']:
         verify_html(start, end)
     elif args['count']:
-        if args['total']:
-            n = count_total(start, end)
-            print "# of days: %d" % n
-        elif args['trade']:
-            n = count_trade(start, end)
-            print "# of trading days: %d" % n
-        elif args['holiday']:
-            n = count_holiday(start, end)
-            print "# of holidays: %d" % n
+        if args['days']:
+            count_days(start, end)
