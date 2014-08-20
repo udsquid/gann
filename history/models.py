ecaee41a8a5224697b22d8fdf6c4a3d51bef9287
udsquid <udsquid.altair@icloud.com>
2014-08-20 08:18:19 +0800

    Add new product: TX

--- history/models.py
+++ history/models.py
@@ -1,16 +1,23 @@
-###
-### django libraries
-###
+#
+# django libraries
+#
 from django.db import models
 
 
-###
-### models
-###
+#
+# models
+#
 class Taiex(models.Model):
     time = models.DateTimeField()
     price = models.DecimalField(max_digits=12, decimal_places=2)
 
+    def __unicode__(self):
+        return "[%s] %s" % (self.time, self.price)
+
+
+class Tx(models.Model):
+    time = models.DateTimeField()
+    price = models.DecimalField(max_digits=12, decimal_places=0)
 
     def __unicode__(self):
         return "[%s] %s" % (self.time, self.price)
