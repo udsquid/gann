0082f52b5184012a4e07486081524aff82c58a69
udsquid <udsquid.altair@icloud.com>
2014-09-27 09:26:57 +0800

    Add completion support

--- mech_sim/IndexGroup.py
+++ mech_sim/IndexGroup.py
@@ -1,3 +1,15 @@
+"""
+Sub-group commands for index operations.
+
+Usage:
+    index product <symbol>
+    index range (start | end) <date> [<time>]
+    index status
+    index (search | searchf) <operator> <value>
+        [(and | or) <operator> <value>]
+"""
+
+
 #
 # project libraries
 #
@@ -8,20 +20,32 @@ from history.models import *
 # module classes
 #
 class IndexGroup():
-    """Usage:
-    index product <symbol>
-    index range (start | end) <date> [<time>]
-    index status
-    index (search | searchf) <operator> <value>
-        [(and | or) <operator> <value>]
-    """
+
+    actions = ['product', 'range', 'status', 'search', 'searchf']
 
     def __init__(self):
         products = ProductInfo.objects.all()
         self.symbols = [p.symbol for p in products]
 
     def complete_command(self, text, line, begin_index, end_index):
-        return [text]
+        if self.has_complete_action(line):
+            return [text]
+        elif not text:
+            return self.actions
+        else:
+            completions = []
+            for act in self.actions:
+                if act.startswith(text):
+                    completions.append(act)
+            return completions
+
+    def has_complete_action(self, line):
+        print '[%s]' % line
+        for act in self.actions:
+            act += ' '
+            if act in line:
+                return True
+        return False
 
     def perform(self, arg):
         print "*** not supported"
