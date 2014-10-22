246b7705f37c4368e8f89b7c0e94e863d7602e3b
udsquid <udsquid.altair@icloud.com>
2014-10-23 01:39:44 +0800

    Add tests for range_start2

--- mech_sim/test_IndexGroup.py
+++ mech_sim/test_IndexGroup.py
@@ -4,9 +4,13 @@
 #
 # libraries
 #
+from datetime import datetime
 import unittest
 
+from django.utils import timezone
+from django.utils.dateparse import parse_datetime
 from docopt import docopt
+import pytz
 
 
 #
@@ -22,10 +26,28 @@ class TestIndexGroup(unittest.TestCase):
     def setUp(self):
         self.index_group = IndexGroup.IndexGroup()
 
+    def _parse_datetime(self, value):
+        naive_datetime = parse_datetime(value)
+        curr_tz = timezone.get_current_timezone_name()
+        return pytz.timezone(curr_tz).localize(naive_datetime)
+
     def test_range_reset(self):
         self.index_group.reset_range()
         self.assertIsNone(self.index_group.range_start)
         self.assertIsNone(self.index_group.range_end)
+
+    def test_set_range_start2_by_str(self):
+        datetime_str = '2014-10-22 01:07:43'
+        self.index_group.range_start2 = datetime_str
+        expected = self._parse_datetime(datetime_str)
+        self.assertEqual(self.index_group.range_start2, expected)
+
+    def test_set_invalid_range_format(self):
+        datetime_str = '2014/10/22 01:07:43'
+        self.assertRaises(ValueError,
+                          self.index_group.range_start2,
+                          datetime_str)
+
 
 #
 # main procedure
