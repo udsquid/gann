0c5da9c1efb08285a03e5d2ee1b488e2638df246
udsquid <udsquid.altair@icloud.com>
2014-11-23 01:49:38 +0800

    Add strategy creation function

--- mech_sim/OrderGroup.py
+++ mech_sim/OrderGroup.py
@@ -3,20 +3,24 @@ Sub-group commands for order operations.
 
 Usage:
     order strategy list
+    order strategy new <name> <symbol>
+    order strategy info <name>
 """
 
 
 #
 # python libraries
 #
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import IntegrityError
 
 
 #
 # project libraries
 #
-from mech_sim.order.models import *
-from lib.exception_decorator import print_except_only
 from auto_complete import *
+from lib.exception_decorator import print_except_only
+from mech_sim.order.models import *
 
 
 #
@@ -38,15 +42,22 @@ class OrderGroup(object):
     @property
     def command_form(self):
         return [['order', 'strategy', 'list'],
+                ['order', 'strategy', 'new', '<name>', '<symbol>'],
+                ['order', 'strategy', 'info', '<name>'],
                 ]
 
     def complete_command(self, text, line, begin_index, end_index):
         return complete_command(text, line, self.command_form)
 
+    @print_except_only
     def perform(self, arg):
         if arg['strategy']:
             if arg['list']:
                 self.show_strategies()
+            elif arg['new']:
+                self.create_strategy_entry(arg['<name>'], arg['<symbol>'])
+            elif arg['info']:
+                self.show_strategy_detail(arg['<name>'])
         else:
             err_msg = "*** should not be here, arg: " + str(arg)
             raise ValueError(err_msg)
@@ -57,3 +68,23 @@ class OrderGroup(object):
         print '-' * 60
         for i, s in enumerate(strategies):
             print "{:>5} | {}".format(i+1, s.name)
+
+    def create_strategy_entry(self, name, symbol):
+        upper_sym = symbol.upper()
+        if upper_sym not in self.symbols:
+            err_msg = "*** unknown symbol: %s" % symbol
+            raise ValueError(err_msg)
+        try:
+            Strategy.objects.create(name=name, symbol=upper_sym)
+        except IntegrityError as e:
+            if 'UNIQUE constraint failed' not in str(e):
+                raise e
+            err_msg = "*** duplicate name: %s" % name
+            raise ValueError(err_msg)
+
+    def show_strategy_detail(self, name):
+        try:
+            Strategy.objects.get(name=name)
+        except ObjectDoesNotExist:
+            err_msg = "*** strategy does not exist: %s" % name
+            raise ValueError(err_msg)
